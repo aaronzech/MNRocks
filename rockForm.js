08ac2313fc25a656a4dbaebeecb2e742f3829d3b
Aaron Zech <aaronzech@gmail.com>
2022-06-01 10:07:09 -0500

    Rock Form updates

--- rockForm.js
+++ rockForm.js
@@ -1,26 +1,37 @@
-window.onload=getExif;
+"use strict"
+const issueForm = document.getElementById('issue-form');
+issueForm.addEventListener('submit', createIssue);
 
-//Grab Form
-const form = document.querySelector('form');
-form.addEventListener('submit', function(e){
-	e.preventDefault();
-	
+// Create a new rock dispaly it to the page
+function createIssue(event) {
+  event.preventDefault();
+  const id = chance.guid();
+  const painterName = document.getElementById('painter-name').value;
+  const filePath1 = document.getElementById('file-path-1').value;
+  const dateHid = document.getElementById('date-hid').value;
+  const filePath2 = document.getElementById('file-path-2').value;
+  const mapLocation = document.getElementById('map-location').value;
+  const status = 'Open';
 
-	// Create Element
-	
-	const name  = document.createElement('DIV');
-	name.textContent=`Name:${form.fname.value}`;
-	document.body.append(name);
-	const datehid  = document.createElement('DIV');
-	datehid.textContent=`Date:${form.found.value}`;
-	document.body.append(datehid);
-	const inpFile = document.getElementById('inpFile');
-	
-	inpFile.addEventListener("change",function(){
-		console.log(inpFile.files);
-	});
-	
-	
-	
-})
+ 
+
+ // Need to work on passing and sharing variables and functions betweeen pages
+  
+  rockPhotos.push(filePath1);
+  rockPhotos.push(filePath2);
+  rockLocations.push(mapLocation);
+
+ 
+
+  console.log(rock);
+
+  issueForm.reset();
+  issueForm.querySelector('#issue-desc').focus();
+}
+
+
+
+
+
+
 
