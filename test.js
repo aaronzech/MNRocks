15346c2d2c62c40a28de722cf292e3e01d9c72a9
Aaron Zech <aaronzech@gmail.com>
2022-06-03 13:57:48 -0500

    New test layout page with Boot strap cards & medium-zoom plugin

---
+++ test.js
@@ -0,0 +1,18 @@
+
+
+// HTML selector
+
+// Zoom in on Photo
+//mediumZoom('.imgSpot',{margin:50});
+
+mediumZoom('.imgSpot', {
+    margin: 30,
+    background: '#c4b6ba',
+    scrollOffset: 0
+  });
+
+  mediumZoom('.map', {
+    margin: 30,
+    background: '#c4b6ba',
+    scrollOffset: 0
+  });
