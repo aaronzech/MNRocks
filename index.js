de654acc7511dc19773b44d69840bd282e9591a7
Aaron Zech <aaronzech@gmail.com>
2022-10-11 13:54:09 -0500

    Add files via upload
    Express - Mongo Integration code added.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const session = require('express-session')
 
 // Mongoose Items
 const mongoose = require('mongoose');
 const { MongoServerClosedError } = require('mongodb');
+const User = require('./Models/user')
+
+const passport = require("passport")
+const LocalStrategy = require('passport-local');
+
 mongoose.connect('mongodb://localhost:27017/mnRocks', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
@@ -14,13 +20,37 @@ mongoose.connect('mongodb://localhost:27017/mnRocks', { useNewUrlParser: true, u
         console.log(err)
     })
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
+
+// Passport Plugins
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 
 app.get('/rock',(req,res) => {
     res.send('Rock from express');
+})
+
+app.get('/newUser',async (req,res) =>{
+    const user = new User({email:'aaronzech@gmail.com', username:'aaronzech'})
+    const newUser = await User.register(user,'admin') // < Password 
+    res.send(newUser)
 })
 
 app.listen(3000,()=>{
